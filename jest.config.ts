486c6401e48148f0905decb4d65074722166e6b1
paytown <paytown@me.com>
2024-04-29 11:53:07 -0400

    fix tests and issues with scss

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ export default {
   testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
-    // process `*.tsx` files with `ts-jest`
+    "^.+\\.scss$": "jest-transform-stub",
   },
   setupFilesAfterEnv: ["jest-fetch-mock"],
 };
